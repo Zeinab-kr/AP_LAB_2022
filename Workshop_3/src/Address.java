7b5d82851da6c4d286582314e515628fe29d9e1b
Zeinab-kr <zeinabk654@gmail.com>
2022-07-01 20:44:59 +0430

    Address class

---
+++ Workshop_3/src/Address.java
@@ -0,0 +1,44 @@
+public class Address {
+    private String zipCode;
+    private String country;
+    private String city;
+
+    // constructor
+    public Address(String zipCode, String country, String city) {
+        this.city = ((city.equals("-")) ? null : city);
+        this.country = ((country.equals("-")) ? null : country);
+        this.zipCode = ((zipCode.equals("-")) ? null : zipCode);
+    }
+
+    public String toString() {
+        return "Email: " + ((zipCode == null) ? "no email!" : zipCode) + "\n"
+                + "Country: " + ((country == null) ? "no country!" : country) + "\n"
+                + "City: " + ((city == null) ? "no city!" : city) + "\n";
+    }
+
+    // setters
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    // getters
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+}
